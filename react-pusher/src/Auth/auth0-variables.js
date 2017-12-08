716d9e969279cf8b44b27050fd94c0259537f1bb
Rohan <rohan@fl769.local>
2017-12-08 14:03:27 +0200

    second commit

---
+++ react-pusher/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: 'garentenent.eu.auth0.com',
+    clientId: 'fZRx5_uxM7jyAXjgzJ8lHpiYujchVV9p',
+    callbackUrl: 'http://localhost:3000/callback'
+    //Callback URL set in the Auth0 dasbhoard.
+}
